aa668bb7897d06725e096de017cfe7857253d05a
daosyn <elmobey@gmail.com>
2018-06-19 00:36:27 -0500

    increase font size to 14

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "tewi:size=12";
+static char *font = "tewi:size=14:antialias=true";
 static int borderpx = 2;
 
 /*
